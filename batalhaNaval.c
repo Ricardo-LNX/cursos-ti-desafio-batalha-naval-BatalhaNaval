1890cfb819e09bde2fedd3ed0581488a7c190a48
Ricardo-LNX <rbernardo.lnx@outlook.com>
2025-11-14 12:55:09 +0000

    Corpo primcipal do código

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -86,97 +86,44 @@ void exibirTabuleiro(int tabuleiro[TAM][TAM]) {
     printf("\n");
 }
 
-
 int main() {
     int tabuleiro[TAM][TAM];
-    int i, j;
 
-    // 1. Inicializar o tabuleiro com água (0)
-    for (i = 0; i < TAM; i++) {
-        for (j = 0; j < TAM; j++) {
-            tabuleiro[i][j] = AGUA;
+    // 1. Inicializar o tabuleiro
+    inicializarTabuleiro(tabuleiro);
+
+    // 2. Posicionar 4 navios de tamanho 3
+    // Navios horizontais/verticais
+    int navios[4][4] = {
+        {2, 1, 0, 0},  // linha 2, coluna 1, horizontal
+        {5, 6, 1, 0},  // linha 5, coluna 6, vertical
+        {0, 0, 0, 1},  // diagonal principal
+        {3, 9, 0, 2}   // diagonal inversa
+    };
+
+    // 3. Posicionar navios no tabuleiro
+    for (int n = 0; n < 4; n++) {
+        int linha = navios[n][0];
+        int coluna = navios[n][1];
+        int orientacao = navios[n][2];
+        int diagonal = navios[n][3];
+
+        if (!posicaoValida(linha, coluna, orientacao, diagonal)) {
+            printf("Erro: navio %d fora dos limites do tabuleiro.\n", n + 1);
+            continue;
         }
+
+        if (sobrepoeNavio(tabuleiro, linha, coluna, orientacao, diagonal)) {
+            printf("Erro: navio %d sobrepõe outro.\n", n + 1);
+            continue;
+        }
+
+        posicionarNavio(tabuleiro, linha, coluna, orientacao, diagonal);
     }
 
-    // 2. Definir coordenadas iniciais dos navios
-    // Coordenadas: (linha, coluna)
-    int linha_horizontal = 2;
-    int coluna_horizontal = 4;
-    int linha_vertical = 6;
-    int coluna_vertical = 1;    
 
-    // 3. Verificar se o navio horizontal cabe no tabuleiro
-    if (coluna_horizontal + NAVIO <= TAM) {
-        for (j = coluna_horizontal; j < coluna_horizontal + NAVIO; j++) {
-            tabuleiro[linha_horizontal][j] = NAVIO_VALOR;
-        }
-    } else {
-        printf("Erro: o navio horizontal excede os limites do tabuleiro.\n");
-        return 1;
-    }
-
-    // 4. Verificar se o navio vertical cabe e não se sobrepõe
-    if (linha_vertical + NAVIO <= TAM) {
-        int sobreposicao = 0;
-        for (i = linha_vertical; i < linha_vertical + NAVIO; i++) {
-            if (tabuleiro[i][coluna_vertical] == NAVIO_VALOR) {
-                sobreposicao = 1;
-                break;
-            }
-        }
-        if (sobreposicao) {
-            printf("Erro: os navios se sobrepõem!\n");
-            return 1;
-        } else {
-            for (i = linha_vertical; i < linha_vertical + NAVIO; i++) {
-                tabuleiro[i][coluna_vertical] = NAVIO_VALOR;
-            }
-        }
-    } else {
-        printf("Erro: o navio vertical excede os limites do tabuleiro.\n");
-        return 1;
-    }
-
-    // 5. Exibir o tabuleiro
-    printf("\n=== TABULEIRO DE BATALHA NAVAL ===\n\n");
-    for (i = 0; i < TAM; i++) {
-        for (j = 0; j < TAM; j++) {
-            printf("%d ", tabuleiro[i][j]);
-        }
-        printf("\n");
-    }
+    // 4. Exibir tabuleiro completo
+    exibirTabuleiro(tabuleiro);
 
     return 0;
-}    
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
-
-
+}
